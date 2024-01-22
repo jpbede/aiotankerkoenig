9b14ddcc0cd2530f45089e07dd8d8f4583017b12
Jan-Philipp Benecke <jan-philipp@bnck.me>
2024-01-22 21:49:31 +0100

    Export invalid key exception

--- aiotankerkoenig/__init__.py
+++ aiotankerkoenig/__init__.py
@@ -2,13 +2,18 @@
 from __future__ import annotations
 
 from .aiotankerkoenig import Tankerkoenig
-from .exceptions import TankerkoenigConnectionError, TankerkoenigError
+from .exceptions import (
+    TankerkoenigConnectionError,
+    TankerkoenigError,
+    TankerkoenigInvalidKeyError,
+)
 from .models import GasType, PriceInfo, Sort, Station, Status
 
 __all__ = [
     "Tankerkoenig",
     "TankerkoenigError",
     "TankerkoenigConnectionError",
+    "TankerkoenigInvalidKeyError",
     "GasType",
     "Sort",
     "Status",
